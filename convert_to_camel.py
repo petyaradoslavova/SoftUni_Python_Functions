e968b3e421de9b708aafe359e438c5271a7d8552
Petya Shopska <145231798+petyaradoslavova@users.noreply.github.com>
2024-02-06 22:43:43 +0200

    Update convert_to_camel.py

--- convert_to_camel.py
+++ convert_to_camel.py
@@ -7,17 +7,4 @@ def camel(string):
     return ''.join(new_list)
 
 
-import re
-
-def to_camel_case(text):
-    words = re.split(r'[-_]', text)
-    words[1:] = list(map(str.capitalize, words[1:]))
-    return "".join(words)
-
-# def to_camel_case(text):
-#     removed = text.replace('-', ' ').replace('_', ' ').split()
-#     if len(removed) == 0:
-#         return ''
-#     return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
-
 print(to_camel_case('the_camel_is_here'))
